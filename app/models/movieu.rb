84a08534bd1a21557fe5d04127a9c0ec4453fd95
J <andadj@gmail.com>
2017-05-05 22:51:30 +0700

    change code in movieu model

--- app/models/movieu.rb
+++ app/models/movieu.rb
@@ -11,5 +11,5 @@ class Movieu < ApplicationRecord
   validates :title, uniqueness: true
   validates :title, presence: true
   has_many :vimages, dependent: :destroy
-  has_many :mstreams, dependent: :destroy
+  
 end
